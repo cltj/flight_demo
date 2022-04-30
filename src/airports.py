b85ab58bc659acb8f23329f7f2bf306bbd9bd0f0
cltj <tj@cloudlink.ai>
2022-04-30 15:09:36 +0200

    new

--- src/airports.py
+++ src/airports.py
@@ -12,45 +12,36 @@ from new_flight import new_guid
 # 5. For each airport find flights within bounding box
 # 6. Return list of flights
 
-class Airports(object):
-    
-    def __init__(self):
-        self.airport_ids = []
-        self.flight_id = []
-
-    def new_guid():
-        """returns a unique id (uuid)"""
-        new_id = uuid.uuid4()
-        return new_id
-
-    
-    def list_airports():
-        """list airports"""
-        return airport_ids
+class Airport():
+    def __init__(self, airport_id, lat, lon):
+        self.airport_id = airport_id
+        self.lat = lat
+        self.lon = lon
 
 
-    def find_flight():
-        """find flight"""
-        return flight_id
-    
-    
-    def add_airport_to_list():
-        """add airport to list"""
-        airport_id = new_guid()
-        return air
+    def uuid_gen(self):
+        """Generates a UUID"""
+        return uuid.uuid4()
 
-    
-    def create_bounding_box(airport_id, tag):
-        connection_string = My_Config.conn_str()
-        azure_table_name = "airports"
-        airport = {"PartitionKey" : airport_id, "RowKey": tag}
 
-        response = entity_crud(connection_string, table_name=azure_table_name, operation='query', entity=airport)
-        if response != None:
-            lamin = str(response['lamin'])
-            lomin = str(response['lomin'])
-            lamax= str(response['lamax'])
-            lomax = str(response['lomax'])
-            return [lamin, lomin, lamax, lomax]
-        else:
-            print("Error: " + str(response))
+    def make_bounding_box(self, lat, lon):
+        """Makes a bounding box from coordinates(lat,lon)"""
+        lamin = lat - 0.7
+        lomin = lon - 0.7
+        lamax = lat + 0.7
+        lomax = lon + 0.7
+        return lamin, lomin, lamax, lomax
+
+
+def add_airport(Airport, lat, lon):
+    """Adds an airport to the airports table"""
+    lamin, lomin, lamax, lomax = Airport().make_bounding_box(lat, lon)
+    airport = [airport_id, tag, lamin, lomin, lamax, lomax]
+    connection_string = My_Config.conn_str()
+    azure_table_name = "airports"
+    response = entity_crud(connection_string, table_name=azure_table_name, operation='insert', entity=airport)
+    if response != None:
+        print("Airport added")
+    else:
+        print("Error: " + str(response))
+
